5fea6bb5ea19bdec68ea84e2e9f5aa2f947365c6
Ferytell <darkstar2709@gmail.com>
2022-06-16 09:30:43 +0700

    this is the one

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-  socketPath: "/cloudsql/capstone-pe4:asia-southeast1:mysql-root-bangkit",
+  
   host: '34.142.212.30', //  Replace with your host name 34.142.172.2 	External 
   user: 'root',      // Replace with your database username 34.128.108.191
   password: 'explosion',      // Replace with your database password
